f96ad1a94eff7bcd5645d363eb16337d862d6991
antlampas <francescoantonetti1@gmail.com>
2023-12-07 16:09:12 +0100

    -

--- testListValidMoves.cpp
+++ testListValidMoves.cpp
@@ -18,30 +18,23 @@ TEST_CASE("List Valid Moves", "[listvalidmoves]")
                         int rowIndex    = int(row[0])    - 49;
                         int columnIndex = int(column[0]) - 97;
 
-                        testingBoard.at(rowIndex).at(columnIndex) = std::string(team)+std::string(piece);
+                        std::cout << "rowIndex: " << rowIndex << std::endl << "columnIndex: " << columnIndex << std::endl << std::endl;
+                        // testingBoard.at(rowIndex).at(columnIndex) = std::string(team)+std::string(piece);
+
+                        // board b(testingBoard);
+
+                        // std::cout << "Coordinates: " << coordinates << std::endl;
+                        // std::cout << "Piece: " << b.getPieceInSquare(coordinates) << std::endl;
                         
-                        // for(auto r: testingBoard)
-                        //     {
-                        //         for(auto c: r)
-                        //             std::cout << c << " ";
-                        //         std::cout << std::endl;
-                        //     }
-                        // std::cout << std::endl;
-
-                        board b(testingBoard);
-
-                        std::vector<std::string> validMoves = b.listValidMoves(coordinates);
-
-                        if(!validMoves.empty())
-                        {
-                            std::cout << "Coordinates: " << coordinates << std::endl;
-                            std::cout << "Piece: " << b.getPieceInSquare(coordinates) << std::endl;
-                            std::cout << "Valid Moves: ";
-                            for(auto move: validMoves)
-                                std::cout << move << " ";
-                            std::cout << std::endl;
-                        }
-
-                        CHECK(!validMoves.empty());
+                        // std::vector<std::string> validMoves = b.listValidMoves(coordinates);
+                        
+                        // if(!validMoves.empty())
+                        // {
+                        //     std::cout << "Valid Moves: ";
+                        //     for(auto move: validMoves)
+                        //         std::cout << move << " ";
+                        //     std::cout << std::endl << std::endl;
+                        // }
+                        // CHECK(!validMoves.empty());
                     }
 }
