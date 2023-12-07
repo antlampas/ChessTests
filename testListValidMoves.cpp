230a3f434596fb1099f2bdb6d18c37f862e14fa3
antlampas <francescoantonetti1@gmail.com>
2023-12-07 17:27:15 +0100

    -

--- testListValidMoves.cpp
+++ testListValidMoves.cpp
@@ -18,25 +18,29 @@ TEST_CASE("List Valid Moves", "[listvalidmoves]")
                         int rowIndex    = int(row[0])    - 49;
                         int columnIndex = int(column[0]) - 97;
 
-                        // std::cout << "rowIndex: " << rowIndex << std::endl << "columnIndex: " << columnIndex << std::endl << std::endl;
                         testingBoard.at(rowIndex).at(columnIndex) = std::string(team)+std::string(piece);
 
                         board b(testingBoard);
 
-                        std::cout << "Coordinates: " << coordinates << std::endl;
-                        std::cout << "Piece: " << b.getPieceInSquare(coordinates) << std::endl;
-                        
                         std::vector<std::string> validMoves = b.listValidMoves(coordinates);
-                        
-                        if(!validMoves.empty())
-                        {
-                            std::cout << "Valid Moves: ";
-                            for(auto move: validMoves)
-                                std::cout << move << " ";
-                            std::cout << std::endl << std::endl;
-                        }
+
+                        // if(!validMoves.empty())
+                        // {
+                        //     std::cout << "Valid Moves: ";
+                        //     for(auto move: validMoves)
+                        //         std::cout << move << " ";
+                        //     std::cout << std::endl << std::endl;
+                        // }
+
                         if(!(piece == "p" && row == "8"))
+                        {
+                            if(validMoves.empty())
+                            {
+                                std::cout << "Coordinates: " << coordinates << std::endl;
+                                std::cout << "Piece: " << b.getPieceInSquare(coordinates) << std::endl;
+                            }
                             CHECK(!validMoves.empty());
+                        }
                         else
                             CHECK(validMoves.empty());
                     }
