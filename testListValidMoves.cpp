457d4c5f6d894ddeb5dca84e495972f6b3708e75
antlampas <francescoantonetti1@gmail.com>
2023-12-07 16:10:32 +0100

    -

--- testListValidMoves.cpp
+++ testListValidMoves.cpp
@@ -7,10 +7,10 @@
 
 TEST_CASE("List Valid Moves", "[listvalidmoves]")
 {
-        for(auto row: {"1","2","3","4","5","6","7","8"})
-            for(auto column: {"a","b","c","d","e","f","g","h"})
-                for(auto team: {"w","b"})
-                    for(auto piece: {"p","r","n","b","k","q"})
+        for(auto piece: {"p","r","n","b","k","q"})
+            for(auto team: {"w","b"})
+                for(auto row: {"1","2","3","4","5","6","7","8"})
+                    for(auto column: {"a","b","c","d","e","f","g","h"})
                     {
                         std::vector<std::vector<std::string>> testingBoard(8,std::vector<std::string>(8,"e"));
                         std::string coordinates = std::string(column)+std::string(row);
