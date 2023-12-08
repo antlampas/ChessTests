76eb995c835a1e9bdb4f18583f00ecaa3ad1289a
antlampas <francescoantonetti1@gmail.com>
2023-12-08 12:06:16 +0100

    -

--- testListValidMoves.cpp
+++ testListValidMoves.cpp
@@ -24,24 +24,8 @@ TEST_CASE("List Valid Moves", "[listvalidmoves]")
 
                         std::vector<std::string> validMoves = b.listValidMoves(coordinates);
 
-                        // if(!validMoves.empty())
-                        // {
-                        //     std::cout << "Valid Moves: ";
-                        //     for(auto move: validMoves)
-                        //         std::cout << move << " ";
-                        //     std::cout << std::endl << std::endl;
-                        // }
-
                         if(!(team == "w" && piece == "p" && row == "8") && !(team == "b" && piece == "p" && row == "1"))
-                        {
-                            if(validMoves.empty())
-                            {
-                                std::cout << "Coordinates: " << coordinates << std::endl;
-                                std::cout << "Piece: " << b.getPieceInSquare(coordinates) << std::endl;
-                                std::cout << b.error << std::endl;
-                            }
                             CHECK(!validMoves.empty());
-                        }
                         else
                             CHECK(validMoves.empty());
                     }
