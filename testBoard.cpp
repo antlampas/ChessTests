c4939b9077e7f00e2c76f6d39d211341df3a12a2
antlampas <francescoantonetti1@gmail.com>
2023-12-10 10:04:08 +0100

    -

--- testBoard.cpp
+++ testBoard.cpp
@@ -13,6 +13,8 @@ TEST_CASE("Board constructor", "[boardconstructor]")
     std::random_device r;
     std::mt19937 g(r());
 
+    std::vector<std::string> whitePawns(8,"wp");
+    std::vector<std::string> blackPawns(8,"bp");
     std::vector<std::string> pieces {"wp","wp","wp","wp","wp","wp","wp","wp","wr","wr","wn","wn","wb","wb","wk","wq","bp","bp","bp","bp","bp","bp","bp","bp","br","br","bn","bn","bb","bb","bk","bq"};
 
     std::shuffle(pieces.begin(),pieces.end(),g);
