9096749f017c4cab9d5a29ef909cfce9f3088f8f
antlampas <francescoantonetti1@gmail.com>
2023-12-11 16:02:58 +0100

    -

--- testIsColumnClogged.cpp
+++ testIsColumnClogged.cpp
@@ -25,5 +25,16 @@ TEST_CASE("Is Column Clogged?", "[iscolumnclogged]")
     
     boardMapType currentBoard = b.boardStatus();
     std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
-    CHECK(false);
+    
+
+    CHECK(b.isColumnClogged("a1","a7"));
+    CHECK(b.isColumnClogged("a1","a4"));
+    CHECK(b.isColumnClogged("a4","a7"));
+    CHECK(b.isColumnClogged("d1","d7"));
+    CHECK(b.isColumnClogged("d1","d4"));
+    CHECK(b.isColumnClogged("d4","d7"));
+
+    CHECK_FALSE(b.isColumnClogged("a1","a3"));
+    CHECK_FALSE(b.isColumnClogged("a4","a6"));
+    CHECK_FALSE(b.isColumnClogged("b1","b7"));
 }
