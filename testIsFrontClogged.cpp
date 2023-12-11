35a9c28db39a9e1a1d034ceb972dae2b74237600
antlampas <francescoantonetti1@gmail.com>
2023-12-11 21:32:48 +0100

    -

--- testIsFrontClogged.cpp
+++ testIsFrontClogged.cpp
@@ -1,12 +1,32 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+#include <algorithm>
 #include "board.hpp"
 
 TEST_CASE("Is Front Clogged?", "[isfrontclogged]")
 {
-    board b;
+    boardMapType boardMap = boardMapType(8,columnType(8,squareType("e")));
 
-    //TODO: Design and Implement
+    boardMap.at(2).at(2) = "br";
+    boardMap.at(5).at(5) = "wk";
+    boardMap.at(4).at(4) = "bq";
+    boardMap.at(2).at(5) = "bp";
+    boardMap.at(5).at(2) = "bp";
+    boardMap.at(0).at(1) = "bp";
+    boardMap.at(0).at(4) = "wp";
+    boardMap.at(6).at(3) = "wp";
+    boardMap.at(7).at(7) = "wp";
+
+    board b(boardMap);
     
-    CHECK(false);
+    boardMapType currentBoard = b.boardStatus();
+    std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
+
+    CHECK(b.isFrontClogged("c2"));
+    CHECK(b.isFrontClogged("h7"));
+    CHECK(b.isFrontClogged("c5"));
+    CHECK_FALSE(b.isFrontClogged("a1"));
+    CHECK_FALSE(b.isFrontClogged("c3"));
+    CHECK_FALSE(b.isFrontClogged("e5"));
 }
--- testIsMoveClogged.cpp
+++ testIsMoveClogged.cpp
@@ -1,12 +1,35 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+#include <algorithm>
 #include "board.hpp"
 
 TEST_CASE("Is Move Clogged?", "[ismoveclogged]")
 {
-    board b;
+    boardMapType boardMap = boardMapType(8,columnType(8,squareType("e")));
 
-    //TODO: Design and Implement
+    boardMap.at(2).at(2) = "br";
+    boardMap.at(5).at(5) = "wk";
+    boardMap.at(4).at(4) = "bq";
+    boardMap.at(2).at(5) = "bp";
+    boardMap.at(5).at(2) = "bp";
+    boardMap.at(0).at(1) = "bp";
+    boardMap.at(0).at(4) = "wp";
+    boardMap.at(6).at(3) = "wp";
+    boardMap.at(7).at(7) = "wp";
+
+    board b(boardMap);
     
-    CHECK(false);
+    boardMapType currentBoard = b.boardStatus();
+    std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
+    
+    CHECK_FALSE(b.isMoveClogged("c3","c8"));
+    CHECK_FALSE(b.isMoveClogged("f6","f7"));
+    CHECK_FALSE(b.isMoveClogged("e5","e6"));
+    CHECK_FALSE(b.isMoveClogged("f3","f4"));
+    CHECK_FALSE(b.isMoveClogged("c6","c7"));
+    CHECK_FALSE(b.isMoveClogged("b1","b2"));
+    CHECK_FALSE(b.isMoveClogged("e1","e2"));
+    CHECK_FALSE(b.isMoveClogged("d7","d8"));
+    CHECK_FALSE(b.isMoveClogged("h8","g8"));
 }
