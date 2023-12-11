6708188e3be1136d3ffd196f50e903c7fb793b5a
antlampas <francescoantonetti1@gmail.com>
2023-12-11 17:43:32 +0100

    -

--- testIsColumnClogged.cpp
+++ testIsColumnClogged.cpp
@@ -1,9 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <utility>
 #include <algorithm>
 #include "board.hpp"
 
@@ -11,10 +8,10 @@ TEST_CASE("Is Column Clogged?", "[iscolumnclogged]")
 {
     boardMapType boardMap = boardMapType(8,columnType(8,squareType("e")));
 
-    boardMap.at(0).at(0) = "wp";
+    boardMap.at(0).at(0) = "wr";
     boardMap.at(3).at(0) = "wp";
-    boardMap.at(7).at(0) = "wp";
-    boardMap.at(0).at(3) = "wp";
+    boardMap.at(7).at(0) = "wq";
+    boardMap.at(0).at(3) = "bp";
     boardMap.at(3).at(3) = "wp";
     boardMap.at(7).at(3) = "wp";
     boardMap.at(0).at(7) = "wp";
@@ -25,7 +22,6 @@ TEST_CASE("Is Column Clogged?", "[iscolumnclogged]")
     
     boardMapType currentBoard = b.boardStatus();
     std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
-    
 
     CHECK(b.isColumnClogged("a1","a7"));
     CHECK(b.isColumnClogged("d1","d7"));
--- testIsDiagonalClogged.cpp
+++ testIsDiagonalClogged.cpp
@@ -1,12 +1,28 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+#include <algorithm>
 #include "board.hpp"
 
 TEST_CASE("Is Diagonal Clogged?", "[isdiagonalclogged]")
 {
-    board b;
+    boardMapType boardMap = boardMapType(8,columnType(8,squareType("e")));
+
+    boardMap.at(2).at(2) = "br";
+    boardMap.at(5).at(5) = "wk";
+    boardMap.at(4).at(4) = "bq";
+    boardMap.at(2).at(5) = "bp";
+    boardMap.at(5).at(2) = "bp";
+    boardMap.at(0).at(1) = "bp";
+    boardMap.at(0).at(4) = "wp";
+    boardMap.at(6).at(3) = "wp";
+    boardMap.at(7).at(7) = "wp";
+
+    board b(boardMap);
     
-    //TODO: Design and Implement
-    
-    CHECK(false);
+    boardMapType currentBoard = b.boardStatus();
+    std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
+
+    CHECK_FALSE(b.isDiagonalClogged("b1","e4"));
+    CHECK(b.isDiagonalClogged("a1","e5"));
 }
