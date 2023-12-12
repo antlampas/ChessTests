c3deb45af4e0f465a89b99fe4fe65e583c8cd202
antlampas <francescoantonetti1@gmail.com>
2023-12-12 17:51:14 +0100

    -

--- testIsFrontClogged.cpp
+++ testIsFrontClogged.cpp
@@ -23,10 +23,10 @@ TEST_CASE("Is Front Clogged?", "[isfrontclogged]")
     boardMapType currentBoard = b.boardStatus();
     std::for_each(currentBoard.rbegin(),currentBoard.rend(),[](columnType column){std::for_each(column.begin(),column.end(),[](squareType square){square!="e" ? std::cout << square << " " : std::cout << square << "  ";}); std::cout << std::endl;});
 
-    CHECK(b.isFrontClogged("c2"));
-    CHECK(b.isFrontClogged("h7"));
-    CHECK(b.isFrontClogged("c5"));
-    CHECK_FALSE(b.isFrontClogged("a1"));
-    CHECK_FALSE(b.isFrontClogged("c3"));
-    CHECK_FALSE(b.isFrontClogged("e5"));
+    CHECK(b.isFrontClogged("c2","c3"));
+    CHECK(b.isFrontClogged("h7","h8"));
+    CHECK(b.isFrontClogged("c5","c6"));
+    CHECK_FALSE(b.isFrontClogged("a1","a2"));
+    CHECK_FALSE(b.isFrontClogged("c3","c4"));
+    CHECK_FALSE(b.isFrontClogged("e5","e6"));
 }
