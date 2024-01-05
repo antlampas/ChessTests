ec43f073b94c341645a6954b431a17bd06078ed4
antlampas <francescoantonetti1@gmail.com>
2024-01-05 12:44:13 +0100

    -

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ if ! [[ -d build ]] then
     mkdir build
 fi
 
+units=""
+
+for flag in "${@:2}"
+do
+    units+=" -D${flag}"
+done
+
+
 cd build
-cmake -DCMAKE_BUILD_TYPE=$2 -DCHESSPREFIX=$1 ..
+cmake -DCMAKE_BUILD_TYPE=$1 ${units} ..
 cd ..
