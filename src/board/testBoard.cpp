23807cdc280d6a7d9ad8a918d363ef2adf44702c
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-03-16 19:19:06 +0100

    -

--- src/board/testBoard.cpp
+++ src/board/testBoard.cpp
@@ -35,7 +35,7 @@ TEST_CASE("Board constructor", "[boardconstructor]")
     for(int i=0;i<8;i++)
     {
         orderedBoard.at(1).at(i) = "wp";
-        orderedBoard.at(6).at(i) = "wp";
+        orderedBoard.at(6).at(i) = "bp";
     }
     
     orderedBoard.at(0) = std::vector<std::string>{"wr","wn","wb","wk","wq","wb","wn","wr"};
