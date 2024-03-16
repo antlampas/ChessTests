ef9a382efd5d8e72783ea3b5613e12e98bef13f5
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-03-16 18:49:18 +0100

    -

--- src/board/testBoard.cpp
+++ src/board/testBoard.cpp
@@ -16,9 +16,9 @@ TEST_CASE("Board constructor", "[boardconstructor]")
     std::vector<std::string> wrongPieces {};
     
     for(int i=0;i<7;i++) pieces.push_back("wp");
-    pieces.insert(pieces.end(),{"wr","wr","wn","wn","wb","wb","wk","wq"});
+    pieces.insert(pieces.end(),{"wr","wn","wb","wk","wq","wb","wn","wr"});
     for(int i=0;i<7;i++) pieces.push_back("bp");
-    pieces.insert(pieces.end(),{"br","br","bn","bn","bb","bb","bk","bq"});
+    pieces.insert(pieces.end(),{"br","bn","bb","bk","bq","bb","bn","br"});
 
     for(int i=0;i<7;i++) wrongPieces.push_back("wp");
     wrongPieces.insert(wrongPieces.end(),{"wr","wr","wn","wn","wb","wb","wk","wq"});
@@ -29,13 +29,30 @@ TEST_CASE("Board constructor", "[boardconstructor]")
 
     board b;
     
-    boardMapType randomBoard = boardMapType(8,columnType(8,squareType("e")));
+    boardMapType orderedBoard = boardMapType(8,columnType(8,squareType("e")));
+    boardMapType randomBoard  = boardMapType(8,columnType(8,squareType("e")));
+
+    for(int i=0;i<8;i++)
+    {
+        orderedBoard.at(1).at(i) = "wp"
+        orderedBoard.at(6).at(i) = "wp"
+    }
+    
+    orderedBoard.at(0) = std::vector<std::string>("wr","wn","wb","wk","wq","wb","wn","wr");
+    orderedBoard.at(0) = std::vector<std::string>("br","bn","bb","bk","bq","bb","bn","br");
+
+    for(auto row: orderedBoard)
+    {
+        for(auto column: row)
+            std::out << column << " ";
+        std::cout << std::endl;
+    }
 
     for(auto p: pieces)
         randomBoard.at(r()%8).at(r()%8) = p;
 
     board c(randomBoard);
-
+    CHECK(b.boardStatus)
     randomBoard.clear();
     randomBoard = boardMapType(8,columnType(8,squareType("e")));
 
