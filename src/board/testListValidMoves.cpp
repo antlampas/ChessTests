3a6b6a32d11580586d72fce562c43797cf5e4f67
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-20 16:09:24 +0200

    -

--- src/board/testListValidMoves.cpp
+++ src/board/testListValidMoves.cpp
@@ -12,34 +12,12 @@ TEST_CASE("List Valid Moves", "[listvalidmoves]")
     boardMapType testingBoard(8,std::vector<std::string>(8,"e"));
     board b(testingBoard);
 
-    for(auto piece: {"p","r","n","b","k","q"})
-        for(auto team: {"w","b"})
-            for(auto row: {"1","2","3","4","5","6","7","8"})
-                for(auto column: {"a","b","c","d","e","f","g","h"})
-                {
-                    std::string coordinates {std::string(column)+std::string(row)};
-
-                    int rowIndex    {int(row[0])    - 49};
-                    int columnIndex {int(column[0]) - 97};
-
-                    testingBoard.at(rowIndex).at(columnIndex) = std::string(team)+std::string(piece);
-
-                    std::vector<std::string> validMoves {b.listValidMoves(coordinates)};
-
-                    if(!(team == "w" && piece == "p" && row == "8") && !(team == "b" && piece == "p" && row == "1"))
-                        CHECK(!validMoves.empty());
-                    else
-                        CHECK(validMoves.empty());
-                }
-    
     std::random_device r;
     std::mt19937 g(r());
 
     std::vector<std::string> pieces {"wp","wp","wp","wp","wp","wp","wp","wp","wr","wr","wn","wn","wb","wb","wk","wq","bp","bp","bp","bp","bp","bp","bp","bp","br","br","bn","bn","bb","bb","bk","bq"};
 
     std::shuffle(pieces.begin(),pieces.end(),g);
-
-    testingBoard = std::move(boardMapType(8,std::vector<std::string>(8,std::string("e"))));
 
     for(auto p: pieces)
         testingBoard.at(r()%8).at(r()%8) = p;
